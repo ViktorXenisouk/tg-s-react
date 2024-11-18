54e0f6e8c05343749aca6b18d2f9b8407facebd0
Viktor Xenisouk <vikktorx2005@gmail.com>
2024-11-18 23:40:31 +0100

    mo[momoi

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,12 +1,11 @@
-import { useCallback, useEffect, useState } from "react"
-import useTelegram from "../hooks/useTelegram";
 import './Form.css'
 
 const Form = () => {
+    /*
     const [country, setCountry] = useState('');
     const [city, setCity] = useState('');
     const [subject, setSubject] = useState('physical');
-   // const { tg } = useTelegram();
+    const { tg } = useTelegram();
 
     const onSendData = useCallback(() => {
         const data = {
@@ -14,35 +13,29 @@ const Form = () => {
             city,
             subject,
         }
-       // tg?.sendData(JSON.stringify(data))
+        tg?.sendData(JSON.stringify(data))
     },[country,city,subject])
 
     useEffect(() => {
-        /*
         tg?.onEvent('mainButtonClicked',onSendData)
         return () => {
             tg?.offEvent('mainButtonClicked',onSendData)
         }
-            */
     },[])
 
     useEffect(() => {
-        /*
         tg?.MainButton.setParams({
             text: 'Send data'
         })
-            */
     }, [])
 
     useEffect(() => {
-        /*
         if (!country || !city) {
             tg?.MainButton.hide();
         }
         else {
             tg?.MainButton.show();
         }
-            */
     }, [country, city])
 
     const onChangeCountry = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -56,22 +49,20 @@ const Form = () => {
     const onChangeSubject = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setSubject(e.target.value);
     }
-
+*/
     return (
         <div className="form">
             <h3>Write your data</h3>
             <input className="input"
                 type="text"
                 placeholder="country"
-                value={country}
-                onChange={onChangeCountry} />
+            />
             <input className="input"
                 type="text"
                 placeholder="city"
-                value={city}
-                onChange={onChangeCity} />
+            />
 
-            <select className="select" value={subject} onChange={onChangeSubject}>
+            <select className="select">
                 <option value="physical">Phys</option>
                 <option value="legal">Legal</option>
             </select>
