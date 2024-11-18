21b38f628a370ffc6c3b37cfe0243b0fe11bc0a9
Viktor Xenisouk <vikktorx2005@gmail.com>
2024-11-18 21:32:14 +0100

    lllllll

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css';
-import ProductList from './components/ProductList/ProductList';
-import Form from './components/Form/Form';
 
 declare global {
   interface Window {
@@ -11,8 +9,7 @@ declare global {
 function App() {
   return (
     <div className='App'>
-      <ProductList />
-      <Form />
+      libhlbi
     </div>
   );
 }
