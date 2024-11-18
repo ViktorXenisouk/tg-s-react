6a98e1bc815a93b82f5854ebfe8b3931195ce21c
Viktor Xenisouk <vikktorx2005@gmail.com>
2024-11-18 18:30:23 +0100

    hofhb

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.css';
 import ProductList from './components/ProductList/ProductList';
 
+declare global {
+  interface Window {
+    Telegram: any;
+  }
+}
+
 function App() {
   return (
     <div className="App">
