06790e8fd84d9eb204b56a326de0b21361007e26
Viktor Xenisouk <vikktorx2005@gmail.com>
2024-11-18 21:28:36 +0100

    gohonnkl

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import './App.css';
 import ProductList from './components/ProductList/ProductList';
 import Form from './components/Form/Form';
-import { Routes, Route } from 'react-router-dom';
 
 declare global {
   interface Window {
